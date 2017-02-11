318db7979b4ff5df1c855ab6152d6fa7401c8584
Christopher Schenk <christopher.schenk@hotmail.com>
2017-02-11 20:11:40 +0100

    Added UI Logic

--- SVP/frmMain.cs
+++ SVP/frmMain.cs
@@ -23,10 +23,7 @@ namespace SVP
             cbProfile.Items.Clear();
             using (svpEntities context = new svpEntities())
             {
-                foreach (var c in context.club)
-                {
-                    cbClub.Items.Add(new ComboboxItem(c.name, c.id));
-                }
+                cbClub.Items.AddRange(context.club.ToArray());
                 foreach (var p in context.profile)
                 {
                     cbProfile.Items.Add(new ComboboxItem(p.name, p.id));
@@ -43,17 +40,27 @@ namespace SVP
         {
             AddUserWizard wizard = new AddUserWizard();
             wizard.ShowDialog();
+            cbClub_SelectedIndexChanged(null, null);
         }
 
         private void btnNewClub_Click(object sender, EventArgs e)
         {
             AddClubWizard wizard = new AddClubWizard();
             wizard.ShowDialog();
+            reload_Controls();
         }
 
         private void cbClub_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (cbClub.SelectedIndex >= 0)
+            {
+                using (svpEntities context = new svpEntities())
+                {
+                    cbMember.Items.Clear();
+                    foreach (member m in context.member.Where(x => x.club_id == ((club)(cbClub.SelectedItem)).id))
+                        cbMember.Items.Add(new ComboboxItem(m.ToString(), m.id));
+                }
+            }
         }
     }
 }
