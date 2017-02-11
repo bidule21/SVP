37c85cace6d70b58969d5f2a04713113936b3bce
Christopher Schenk <christopher.schenk@hotmail.com>
2017-02-11 21:23:36 +0100

    Seperated Tab from Main Form

---
+++ SVP/TabTraining.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DisagLib;
+
+namespace SVP
+{
+    public partial class TabTraining : UserControl
+    {
+        public TabTraining()
+        {
+            InitializeComponent();
+        }
+        private void reload_Controls()
+        {
+            cbClub.Items.Clear();
+            cbProfile.Items.Clear();
+            using (svpEntities context = new svpEntities())
+            {
+                cbClub.Items.AddRange(context.club.ToArray());
+                foreach (var p in context.profile)
+                {
+                    cbProfile.Items.Add(new ComboboxItem(p.name, p.id));
+                }
+            }
+        }
+
+        private List<RMResult> readShots(string profile)
+        {
+            DISAGRM machine = new DISAGRM();
+            try
+            {
+                machine.Start(SVP.Properties.Settings.Default.ComPort);
+                List<RMResult> results = machine.GetShots(profile);
+                return results;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fehler beim einlesen der Ergebnisse:\r\n" + e.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                machine.Stop();
+            }
+            return null;
+        }
+
+        private void btnNewMember_Click(object sender, EventArgs e)
+        {
+            AddUserWizard wizard = new AddUserWizard();
+            wizard.ShowDialog();
+            cbClub_SelectedIndexChanged(null, null);
+        }
+
+        private void btnNewClub_Click(object sender, EventArgs e)
+        {
+            AddClubWizard wizard = new AddClubWizard();
+            wizard.ShowDialog();
+            reload_Controls();
+        }
+
+        private void cbClub_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbClub.SelectedIndex >= 0)
+            {
+                using (svpEntities context = new svpEntities())
+                {
+                    cbMember.Items.Clear();
+                    foreach (member m in context.member.Where(x => x.club_id == ((club)(cbClub.SelectedItem)).id))
+                        cbMember.Items.Add(new ComboboxItem(m.ToString(), m.id));
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            gbRead.Enabled = true;
+            lblClub.Text = cbClub.SelectedItem.ToString();
+            lblMember.Text = cbMember.SelectedItem.ToString();
+            profile p = null;
+            using (svpEntities context = new svpEntities())
+                p = context.profile.Where(x => x.id == SVP.Properties.Settings.Default.DefaultProfile).FirstOrDefault();
+            if (p != null)
+                cbProfile.SelectedIndex = cbProfile.FindStringExact(p.name);
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            using (svpEntities context = new svpEntities())
+            {
+                disagprofile profile = context.disagprofile.Where(x => x.profile_id == ((ComboboxItem)cbProfile.SelectedItem).Id).First();
+                System.Threading.Tasks.Task<List<RMResult>> ta = System.Threading.Tasks.Task.Factory.StartNew<List<RMResult>>(() => readShots(profile.value));
+                while (!ta.IsCompleted)
+                {
+                    Application.DoEvents();
+                }
+                if (ta.Result == null)
+                {
+                    btnRead.Enabled = true;
+                    return;
+                }
+                sequence sequence = new sequence();
+                sequence.date = DateTime.Now;
+                sequence.member_id = ((ComboboxItem)cbMember.SelectedItem).Id;
+                sequence.profile_id = ((ComboboxItem)cbProfile.SelectedItem).Id;
+
+                foreach (RMResult result in ta.Result)
+                {
+                    shot s = new shot();
+                    s.value = result.Rings;
+                    s.angle = result.Angle;
+                    s.factor_value = result.FactorValue;
+                    s.shot_number = result.ShotNumber;
+                    s.valid = (result.Validity == ValidFlag.Valid);
+                    sequence.shot.Add(s);
+                }
+                context.sequence.Add(sequence);
+                context.SaveChanges();
+
+            }
+        }
+
+        private void TabTraining_Load(object sender, EventArgs e)
+        {
+            reload_Controls();
+        }
+    }
+}
--- SVP/frmMain.Designer.cs
+++ SVP/frmMain.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SVP
+﻿using System;
+
+namespace SVP
 {
     partial class frmMain
     {
@@ -30,259 +32,53 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tpTraining = new System.Windows.Forms.TabPage();
-            this.gbRead = new System.Windows.Forms.GroupBox();
-            this.btnReRead = new System.Windows.Forms.Button();
-            this.btnRead = new System.Windows.Forms.Button();
-            this.lblMember = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lblClub = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cbProfile = new System.Windows.Forms.ComboBox();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnNewMember = new System.Windows.Forms.Button();
-            this.btnNewClub = new System.Windows.Forms.Button();
-            this.cbMember = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cbClub = new System.Windows.Forms.ComboBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dgResults = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
-            this.tpTraining.SuspendLayout();
-            this.gbRead.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tpTraining);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(428, 595);
+            this.tabControl1.Size = new System.Drawing.Size(836, 595);
             this.tabControl1.TabIndex = 0;
             // 
             // tpTraining
             // 
-            this.tpTraining.Controls.Add(this.gbRead);
-            this.tpTraining.Controls.Add(this.btnStart);
-            this.tpTraining.Controls.Add(this.btnNewMember);
-            this.tpTraining.Controls.Add(this.btnNewClub);
-            this.tpTraining.Controls.Add(this.cbMember);
-            this.tpTraining.Controls.Add(this.label2);
-            this.tpTraining.Controls.Add(this.label1);
-            this.tpTraining.Controls.Add(this.cbClub);
+            this.tpTraining.Controls.Add(new TabTraining());
             this.tpTraining.Location = new System.Drawing.Point(4, 22);
             this.tpTraining.Name = "tpTraining";
             this.tpTraining.Padding = new System.Windows.Forms.Padding(3);
-            this.tpTraining.Size = new System.Drawing.Size(420, 569);
+            this.tpTraining.Size = new System.Drawing.Size(828, 569);
             this.tpTraining.TabIndex = 0;
             this.tpTraining.Text = "Trainingsschießen";
             this.tpTraining.UseVisualStyleBackColor = true;
-            // 
-            // gbRead
-            // 
-            this.gbRead.Controls.Add(this.btnReRead);
-            this.gbRead.Controls.Add(this.btnRead);
-            this.gbRead.Controls.Add(this.lblMember);
-            this.gbRead.Controls.Add(this.label5);
-            this.gbRead.Controls.Add(this.lblClub);
-            this.gbRead.Controls.Add(this.label4);
-            this.gbRead.Controls.Add(this.label3);
-            this.gbRead.Controls.Add(this.cbProfile);
-            this.gbRead.Enabled = false;
-            this.gbRead.Location = new System.Drawing.Point(11, 91);
-            this.gbRead.Name = "gbRead";
-            this.gbRead.Size = new System.Drawing.Size(400, 175);
-            this.gbRead.TabIndex = 9;
-            this.gbRead.TabStop = false;
-            this.gbRead.Text = "Einlesen";
-            // 
-            // btnReRead
-            // 
-            this.btnReRead.Enabled = false;
-            this.btnReRead.Location = new System.Drawing.Point(301, 139);
-            this.btnReRead.Name = "btnReRead";
-            this.btnReRead.Size = new System.Drawing.Size(93, 23);
-            this.btnReRead.TabIndex = 13;
-            this.btnReRead.Text = "Nachwertung";
-            this.btnReRead.UseVisualStyleBackColor = true;
-            // 
-            // btnRead
-            // 
-            this.btnRead.Location = new System.Drawing.Point(301, 110);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(93, 23);
-            this.btnRead.TabIndex = 12;
-            this.btnRead.Text = "Einlesen";
-            this.btnRead.UseVisualStyleBackColor = true;
-            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
-            // 
-            // lblMember
-            // 
-            this.lblMember.AutoSize = true;
-            this.lblMember.Location = new System.Drawing.Point(113, 86);
-            this.lblMember.Name = "lblMember";
-            this.lblMember.Size = new System.Drawing.Size(10, 13);
-            this.lblMember.TabIndex = 11;
-            this.lblMember.Text = "-";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 86);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Schütze / Schützin:";
-            // 
-            // lblClub
-            // 
-            this.lblClub.AutoSize = true;
-            this.lblClub.Location = new System.Drawing.Point(113, 60);
-            this.lblClub.Name = "lblClub";
-            this.lblClub.Size = new System.Drawing.Size(10, 13);
-            this.lblClub.TabIndex = 7;
-            this.lblClub.Text = "-";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 60);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Verein:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Profil:";
-            // 
-            // cbProfile
-            // 
-            this.cbProfile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbProfile.FormattingEnabled = true;
-            this.cbProfile.Location = new System.Drawing.Point(116, 24);
-            this.cbProfile.Name = "cbProfile";
-            this.cbProfile.Size = new System.Drawing.Size(185, 21);
-            this.cbProfile.TabIndex = 0;
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(318, 62);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(93, 23);
-            this.btnStart.TabIndex = 8;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // btnNewMember
-            // 
-            this.btnNewMember.Location = new System.Drawing.Point(318, 33);
-            this.btnNewMember.Name = "btnNewMember";
-            this.btnNewMember.Size = new System.Drawing.Size(93, 23);
-            this.btnNewMember.TabIndex = 7;
-            this.btnNewMember.Text = "Neuer Schütze";
-            this.btnNewMember.UseVisualStyleBackColor = true;
-            this.btnNewMember.Click += new System.EventHandler(this.btnNewMember_Click);
-            // 
-            // btnNewClub
-            // 
-            this.btnNewClub.Location = new System.Drawing.Point(318, 6);
-            this.btnNewClub.Name = "btnNewClub";
-            this.btnNewClub.Size = new System.Drawing.Size(93, 23);
-            this.btnNewClub.TabIndex = 6;
-            this.btnNewClub.Text = "Neuer Verein";
-            this.btnNewClub.UseVisualStyleBackColor = true;
-            this.btnNewClub.Click += new System.EventHandler(this.btnNewClub_Click);
-            // 
-            // cbMember
-            // 
-            this.cbMember.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.cbMember.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cbMember.FormattingEnabled = true;
-            this.cbMember.Location = new System.Drawing.Point(117, 33);
-            this.cbMember.Name = "cbMember";
-            this.cbMember.Size = new System.Drawing.Size(195, 21);
-            this.cbMember.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Schütze / Schützin:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Verein:";
-            // 
-            // cbClub
-            // 
-            this.cbClub.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.cbClub.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cbClub.FormattingEnabled = true;
-            this.cbClub.Location = new System.Drawing.Point(117, 6);
-            this.cbClub.Name = "cbClub";
-            this.cbClub.Size = new System.Drawing.Size(195, 21);
-            this.cbClub.TabIndex = 1;
-            this.cbClub.SelectedIndexChanged += new System.EventHandler(this.cbClub_SelectedIndexChanged);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(420, 569);
+            this.tabPage2.Size = new System.Drawing.Size(828, 569);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // dgResults
-            // 
-            this.dgResults.AllowUserToAddRows = false;
-            this.dgResults.AllowUserToDeleteRows = false;
-            this.dgResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgResults.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dgResults.Location = new System.Drawing.Point(430, 0);
-            this.dgResults.Name = "dgResults";
-            this.dgResults.ReadOnly = true;
-            this.dgResults.Size = new System.Drawing.Size(406, 595);
-            this.dgResults.TabIndex = 1;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(836, 595);
-            this.Controls.Add(this.dgResults);
             this.Controls.Add(this.tabControl1);
             this.Name = "frmMain";
             this.Text = "frmMain";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.tabControl1.ResumeLayout(false);
-            this.tpTraining.ResumeLayout(false);
-            this.tpTraining.PerformLayout();
-            this.gbRead.ResumeLayout(false);
-            this.gbRead.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -292,22 +88,5 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tpTraining;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cbClub;
-        private System.Windows.Forms.ComboBox cbProfile;
-        private System.Windows.Forms.Button btnNewMember;
-        private System.Windows.Forms.Button btnNewClub;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox cbMember;
-        private System.Windows.Forms.GroupBox gbRead;
-        private System.Windows.Forms.Button btnReRead;
-        private System.Windows.Forms.Button btnRead;
-        private System.Windows.Forms.Label lblMember;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label lblClub;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.DataGridView dgResults;
     }
 }
