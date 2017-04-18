f2c4746f74aee4351540fe8be6b65783398e6726
Chris <Chris@DESKTOP-JTRJRRI>
2017-04-18 20:17:54 +0200

    Added different DisplayLevels to the Monitor

--- SVP/TabSettings.cs
+++ SVP/TabSettings.cs
@@ -22,5 +22,22 @@ namespace SVP
             ProfileWizard wizard = new ProfileWizard();
             wizard.ShowDialog();
         }
-    }
+
+		private void rbDisplay_CheckedChanged(object sender, EventArgs e)
+		{
+			btnApplayDisplaySetting.Enabled = true;
+		}
+
+		private void btnApplayDisplaySetting_Click(object sender, EventArgs e)
+		{
+			if (rbDisplayEverything.Checked)
+				Monitor.GetMonitor().SetDisplaySetting(DisplaySetting.Everything);
+			else if (rbDisplayEverythingAnonym.Checked)
+				Monitor.GetMonitor().SetDisplaySetting(DisplaySetting.EverythingAnonym);
+			else if (rbDisplayShotImageWithPoints.Checked)
+				Monitor.GetMonitor().SetDisplaySetting(DisplaySetting.ShotImageWithPoints);
+			else if (rbDisplayShotImage.Checked)
+				Monitor.GetMonitor().SetDisplaySetting(DisplaySetting.ShotImage);
+		}
+	}
 }
