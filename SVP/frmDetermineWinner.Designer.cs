a68ac0340caeb81a42220c136dc06219cdfb6609
Christopher Schenk <geschenkterchris@gmx.de>
2017-04-27 21:10:37 +0200

    Added Workflow to choose sequences which will be reevaluated

--- SVP/frmDetermineWinner.Designer.cs
+++ SVP/frmDetermineWinner.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.dvResultList = new System.Windows.Forms.DataGridView();
             this.btnOk = new System.Windows.Forms.Button();
+            this.placement = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memberName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Points = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reevaluate = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dvResultList)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,11 +41,18 @@
             // 
             this.dvResultList.AllowUserToAddRows = false;
             this.dvResultList.AllowUserToDeleteRows = false;
+            this.dvResultList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dvResultList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvResultList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.placement,
+            this.memberName,
+            this.Points,
+            this.Reevaluate});
             this.dvResultList.Dock = System.Windows.Forms.DockStyle.Top;
             this.dvResultList.Location = new System.Drawing.Point(0, 0);
             this.dvResultList.Name = "dvResultList";
             this.dvResultList.ReadOnly = true;
+            this.dvResultList.RowHeadersVisible = false;
             this.dvResultList.Size = new System.Drawing.Size(667, 204);
             this.dvResultList.TabIndex = 0;
             // 
@@ -53,6 +64,31 @@
             this.btnOk.TabIndex = 1;
             this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // placement
+            // 
+            this.placement.HeaderText = "Plazierung";
+            this.placement.Name = "placement";
+            this.placement.ReadOnly = true;
+            // 
+            // memberName
+            // 
+            this.memberName.HeaderText = "Name";
+            this.memberName.Name = "memberName";
+            this.memberName.ReadOnly = true;
+            // 
+            // Points
+            // 
+            this.Points.HeaderText = "Ringe";
+            this.Points.Name = "Points";
+            this.Points.ReadOnly = true;
+            // 
+            // Reevaluate
+            // 
+            this.Reevaluate.HeaderText = "Erneut werten?";
+            this.Reevaluate.Name = "Reevaluate";
+            this.Reevaluate.ReadOnly = true;
             // 
             // frmDetermineWinner
             // 
@@ -75,5 +111,9 @@
 
         private System.Windows.Forms.DataGridView dvResultList;
         private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn placement;
+        private System.Windows.Forms.DataGridViewTextBoxColumn memberName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Points;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Reevaluate;
     }
 }
--- SVP/frmDetermineWinner.cs
+++ SVP/frmDetermineWinner.cs
@@ -12,10 +12,12 @@ namespace SVP
 {
     public partial class frmDetermineWinner : Form
     {
+        private price price;
         public frmDetermineWinner(price p)
         {
             InitializeComponent();
             this.Text += " " + p.name;
+            this.price = p;
         }
 
         public frmDetermineWinner(award a)
@@ -24,9 +26,40 @@ namespace SVP
             this.Text += " " + a.name;
         }
 
+        //TODO: swap member with participant to add group support
         private void frmDetermineWinner_Load(object sender, EventArgs e)
         {
+            using (svpEntities context = new svpEntities())
+            {
+                var bestResult = context.price.Include("sequence").FirstOrDefault(x => x.id == price.id).sequence.Max(x => x.shot.Sum(y => y.value));
+                int count = context.price.Include("sequence").FirstOrDefault(x => x.id == price.id).sequence.Where(x => x.shot.Sum(y => y.value) == bestResult).Count();
+                int place = 1;
+                foreach (var sequence in context.price.Include("sequence").Include("member").Include("shot").FirstOrDefault(x => x.id == price.id).sequence.OrderBy(x => x.shot.Sum(y => y.value)))
+                {
+                    bool reevaluateSeq = false;
+                    sequence seq = (sequence.sequence1.Count() == 0) ? sequence : sequence.sequence1.First();
+                    if(count > 1)
+                    {
+                        reevaluateSeq = true;
+                    }else if (count < 1)
+                    {
+                        throw new Exception("Something really weird happend: found no Sequences with the Best Result");
+                    }
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.Tag = sequence.id;
+                    row.Cells.Add(new DataGridViewTextBoxCell() { Value = place++ });
+                    row.Cells.Add(new DataGridViewTextBoxCell() { Value = sequence.member.name });
+                    row.Cells.Add(new DataGridViewTextBoxCell() { Value = sequence.shot.Sum(x => x.value )});
+                    row.Cells.Add(new DataGridViewCheckBoxCell() { Value = reevaluateSeq });
+                    dvResultList.Rows.Add(row);
+                }
 
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //TODO: reevaluate the checked Rows
         }
     }
 }
